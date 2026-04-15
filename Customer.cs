a94455bb88871e726e769d65413fa2c5ed26daaa
Viqo07 <vicomus19@gmail.com>
2026-04-14 19:57:42 -0500

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+public class Customer
+{
+	// Primary Key
+	public int CustomerId { get; set; }
+
+	// Name (required, max 120)
+	[Required]
+	[MaxLength(120)]
+	public string Name { get; set; } = string.Empty;
+
+	// Email (required, max 120)
+	[Required]
+	[MaxLength(120)]
+	public string Email { get; set; } = string.Empty;
+
+	// Navigation property (One-to-Many)
+	public List<Order> Orders { get; set; } = new List<Order>();
+}
